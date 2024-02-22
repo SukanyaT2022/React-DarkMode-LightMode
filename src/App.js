75c5b8e692be6881ae66b736e2c5865629db5d65
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-02-22 13:04:29 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // let changeColor ={
+  //   color:'pink',
+  //   fontSize: '25px'
+  // }
+  const [colorState,setColorState] = useState('black')
+  const [flag,setFlag] = useState(false);
+
+  useEffect(()=>{
+    flag?setColorState('red'):setColorState('black')
+  },[flag])
+
+  const switchHandler=()=>{
+    setFlag(!flag)
+    console.log(flag)
+  }
+
+  return(
+<div>
+ <label className={`switch ${colorState}`} onClick={switchHandler}>
+   <input type="checkbox"/>
+   <span className="slider round"></span>
+ </label>
+ <p style={{color:colorState}}>Bye Bye Winter!</p>
+ {/* //color is inline css property */}
+</div>
+
+  )
+
+}
+
+export default App;
+
+
 // let changeColor = {
 //   color: 'red',
 //   fontSize: '20px'
 // }
 
-const [changeTextColor,setChangeTextColor] = useState('black')
+// const [changeTextColor,setChangeTextColor] = useState('black')
 
 
-  return (
+//   return (
   
-    <div className="App">
+//     <div className="App">
     
-<label className="switch">
-  <input type="checkbox"/>
-  <span className="slider round"></span>
-</label>
-      {/* <p style={changeColor}>Hello Sunshine!</p> */}
+// <label className="switch">
+//   <input type="checkbox"/>
+//   <span className="slider round"></span>
+// </label>
+//       {/* <p style={changeColor}>Hello Sunshine!</p> */}
 
-      <h2 style={{color:changeTextColor}}>Hello Summer!</h2>
+//       <h2 style={{color:changeTextColor}}>Hello Summer!</h2>
     
-    </div>
-  );
-}
-
-export default App;
+//     </div>
+//   );
